85e2487479aff4a1ed9ee91e46974746577b254e
kumarpraveen-s <praveenkumars2831@gmail.com>
2025-04-07 01:38:38 +0530

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
@@ -14,7 +13,6 @@ import { protectRoute } from "./middleware/protectRoute.js";
 const app = express();
 
 const PORT = ENV_VARS.PORT;
-const __dirname = path.resolve();
 
 app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
@@ -24,20 +22,20 @@ app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
-if (ENV_VARS.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "backend/public")));
-    // Enhanced catch-all with error handling
-    const filePath = path.join(__dirname, "backend", "public", "index.html");
-    console.log("Attempting to serve:", filePath);
-    app.get("/*", (req, res) => {
-        res.sendFile(filePath, (err) => {
-            if (err) {
-                console.error("Error serving index.html:", err);
-                res.status(500).send("Error loading page");
-            }
-        });
-    });
-}
+// if (ENV_VARS.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "backend/public")));
+//     // Enhanced catch-all with error handling
+//     const filePath = path.join(__dirname, "backend", "public", "index.html");
+//     console.log("Attempting to serve:", filePath);
+//     app.get("/*", (req, res) => {
+//         res.sendFile(filePath, (err) => {
+//             if (err) {
+//                 console.error("Error serving index.html:", err);
+//                 res.status(500).send("Error loading page");
+//             }
+//         });
+//     });
+// }
 
 app.listen(PORT, () => {
     console.log("Server started at http://localhost:" + PORT);
