09831c14c93488748ba973b4ed0be1d6792b22c1
kumarpraveen-s <praveenkumars2831@gmail.com>
2025-04-07 15:09:04 +0530

    fixed bugs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        proxy: {
-            "/api": {
-                target: "https://netflix-clone-yw5d.onrender.com",
-            },
-        },
-    },
+    // server: {
+    //     proxy: {
+    //         "/api": {
+    //             target: "https://netflix-clone-yw5d.onrender.com",
+    //         },
+    //     },
+    // },
 });
